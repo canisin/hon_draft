943d9a614868e83ad1e78b4842280f733f79c613
can.isin <can.isin@paradoxinteractive.com>
2024-09-27 14:28:50 +0200

    Reimplements the background updates as a daemon thread.

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
-from threading import Timer
-import atexit
+from threading import Thread
+from time import sleep
 
 app = Flask( __name__ )
 socketio = SocketIO( app )
 
 state = { "value": 0 }
-updateTimer = None
+
+def logic():
+    print( "starting logic thread" )
+    while True:
+        sleep( 1 )
+        update()
 
 def update():
     state[ "value" ] += 1
-
-    startUpdates()
-
-def startUpdates():
-    global updateTimer
-    updateTimer = Timer( 1, update )
-    updateTimer.start()
-
-def cancelUpdates():
-    updateTimer.cancel()
 
 @app.route( "/" )
 def home():
     return f"Hello world! { state[ "value" ] }"
 
 if __name__ == "__main__":
-    startUpdates()
-    atexit.register( cancelUpdates )
+    Thread( target = logic, daemon = True ).start()
 
-    socketio.run( app, host = "localhost", port = 80, debug = True )
+    socketio.run( app, host = "localhost", port = 80 )
