e8ce11a3ec7e029246e737ec2f5bacd2fbea081b
can.isin <can.isin@paradoxinteractive.com>
2024-09-27 13:24:28 +0200

    Adds hello world example.

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ from flask_socketio import SocketIO
 app = Flask( __name__ )
 socketio = SocketIO( app )
 
+@app.route( "/" )
+def home():
+    return "Hello world!"
+
 if __name__ == "__main__":
     socketio.run( app, host = "localhost", port = 80, debug = True )
