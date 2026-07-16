778bbf6fa42b232a75490974ea249afb7d2af937
canisin <canisin@users.noreply.github.com>
2026-07-16 19:41:59 +0200

    Adds a help command.

--- commands.py
+++ commands.py
@@ -5,6 +5,42 @@ import messages
 import draft
 import utils
 
+class Command:
+    def __init__( self, command, help ):
+        self.command = command
+        self.help = help
+
+    def connect( self, func ):
+        self.func = func
+
+    def execute( self, player, parameters ):
+        self.func( player, parameters )
+
+commands = []
+
+def command_decorator( command, help ):
+    command = Command( command, help )
+    commands.append( command )
+    return command.connect
+
+@command_decorator( "help", "prints help" )
+def print_help( player, parameters ):
+    for command in commands:
+        messages.emit_message( f"<span style=\"font-family: monspace, monospace; color: blue\">/{ command.command }</span>: { command.help }", to = request.sid )
+
+@command_decorator( "name", "sets player name" )
+def set_name( player, name ):
+    if not name: return
+    name = name[:16]
+    # tell the client to make a request to set the cookie
+    emit( "set-name", name )
+
+@command_decorator( "reset", "resets the server" )
+def reset_server( player, parameters ):
+    utils.log( "resetting server" )
+    draft.reset_draft( clear_players = True )
+    messages.emit_message( f"<span style=\"color: red\">{ player.get_formatted_name() } has reset the server, please refresh the page.</span>" )
+
 def try_dispatch( player, message ):
     if message[:1] != "/": return False
     ( command, _, parameters ) = message[1:].partition( " " )
@@ -12,21 +48,8 @@ def try_dispatch( player, message ):
     return True
 
 def dispatch( player, command, parameters ):
-    match command:
-        case "name":
-            set_name( parameters )
-        case "reset":
-            reset_server( player )
-        case _:
+    command = next( c for c in commands if c.command == command )
+    if not command:
             messages.emit_message( "<span style=\"color: red\">Unrecognized command</span>", to = request.sid )
-
-def set_name( name ):
-    if not name: return
-    name = name[:16]
-    # tell the client to make a request to set the cookie
-    emit( "set-name", name )
-
-def reset_server( player ):
-    utils.log( "resetting server" )
-    draft.reset_draft( clear_players = True )
-    messages.emit_message( f"<span style=\"color: red\">{ player.get_formatted_name() } has reset the server, please refresh the page.</span>" )
+            return
+    command.execute( player, parameters )
